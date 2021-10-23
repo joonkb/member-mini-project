e8ca9794182b399fce3716a45b44cc8d812cb1f8
Yong Lee <nogy21@gmail.com>
2021-10-23 22:06:59 +0900

    Fix: member-db.sql

--- src/main/webapp/WEB-INF/info/member-db.sql
+++ src/main/webapp/WEB-INF/info/member-db.sql
@@ -1,13 +1,16 @@
-	create table mvc_member(
-			id varchar2(100) primary key,
-			email varchar2(100) not null, 
-			password varchar2(100) not null,
-			name varchar2(100) not null,
-			address varchar2(100) not null,
-			birthday date not null,
-			regdate date not null,
-			question_no number not null,
-			answer varchar2(100) not null,
+create table mini_member(
+	id varchar2(100) primary key,
+	email varchar2(100) not null, 
+	password varchar2(100) not null,
+	name varchar2(100) not null,
+	address varchar2(100) not null,
+	birthday date not null,
+	regdate date not null,
+	question_no number not null,
+    answer varchar2(100) not null
 )
 
-insert into mvc_member values(id, email, password, name, address, birthday, regdate, question_no, answer)
+insert into mini_member(id, email, password, name, address, birthday, regdate, question_no, answer)
+    values('test','test@kst.com','0000','테스트','한국','1900-01-01',sysdate,'1','컴퓨터');
+
+select * from mini_member;
