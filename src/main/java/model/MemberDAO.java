83dd64fb18ee5e7a9c53a29306010da9fabb953a
Yong Lee <nogy21@gmail.com>
2021-10-24 18:03:06 +0900

    Fix typo

--- src/main/java/model/MemberDAO.java
+++ src/main/java/model/MemberDAO.java
@@ -8,26 +8,26 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class MemberDAO {
-		private static MemberDAO instance=new MemberDAO();
-		private String url="jdbc:oracle:thin:@127.0.0.1:1521:xe";
-		private String username="scott";
-		private String userpass="tiger";
-		private MemberDAO() {}
-		public static MemberDAO getInstance() {
-			return instance;
-		}
-		//closeAll 오버로딩 2개 만든다 
-		public void closeAll(PreparedStatement pstmt,Connection con) throws SQLException {
-			if(pstmt!=null)
-				pstmt.close();
-			if(con!=null)
-				con.close();
-		}
-		public void closeAll(ResultSet rs,PreparedStatement pstmt,Connection con) throws SQLException {
-			if(rs!=null)
-				rs.close();
-			closeAll(pstmt, con);
-		}
+	private static MemberDAO instance=new MemberDAO();
+	private String url="jdbc:oracle:thin:@127.0.0.1:1521:xe";
+	private String username="scott";
+	private String userpass="tiger";
+	private MemberDAO() {}
+	public static MemberDAO getInstance() {
+		return instance;
+	}
+	//closeAll 오버로딩 2개 만든다 
+	public void closeAll(PreparedStatement pstmt,Connection con) throws SQLException {
+		if(pstmt!=null)
+			pstmt.close();
+		if(con!=null)
+			con.close();
+	}
+	public void closeAll(ResultSet rs,PreparedStatement pstmt,Connection con) throws SQLException {
+		if(rs!=null)
+			rs.close();
+		closeAll(pstmt, con);
+	}
     public ArrayList<MemberVO> findMemberList() throws SQLException {
         ArrayList<MemberVO> list = new ArrayList<MemberVO>();
         Connection con=null;
@@ -54,50 +54,63 @@ public class MemberDAO {
         return list;
     }
     public MemberVO login(String id, String password) throws SQLException {
-    MemberVO vo = null;
-    Connection con = null;
-    PreparedStatement pstmt = null;
-    ResultSet rs = null;
-    try {
-      con = DriverManager.getConnection(url, username, password);
-      String sql = "select * from mini_member where id = ? and password = ?";
-      pstmt = con.prepareStatement(sql);
-      pstmt.setString(1, id);
-      pstmt.setString(2, password);
-      rs = pstmt.executeQuery();
-      if(rs.next()) {
-        vo = new MemberVO(rs.getString(1), rs.getString(2), rs.getString(3),
-            rs.getString(4), rs.getString(5), rs.getString(6),
-            rs.getString(7), rs.getInt(8), rs.getString(9));
-      }
-    } finally {
-      closeAll(rs, pstmt, con);
+        MemberVO vo = null;
+        Connection con = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        try {
+            con = DriverManager.getConnection(url, username, password);
+            String sql = "select * from mini_member where id = ? and password = ?";
+            pstmt = con.prepareStatement(sql);
+            pstmt.setString(1, id);
+            pstmt.setString(2, password);
+            rs = pstmt.executeQuery();
+            if(rs.next()) {
+                vo = new MemberVO(rs.getString(1), rs.getString(2), rs.getString(3),
+                    rs.getString(4), rs.getString(5), rs.getString(6),
+                    rs.getString(7), rs.getInt(8), rs.getString(9));
+            }
+        } finally {
+            closeAll(rs, pstmt, con);
+        }
+        return vo;
     }
-    return vo;
-  }
-  public void register(MemberVO vo) throws SQLException {
-            
-  }
-        
-        
-  public boolean idCheck(String id) throws SQLException {
-      boolean result = false;
-      Connection con = null;
-      PreparedStatement pstmt =  null;
-      ResultSet rs = null;
-      try {
-          con = DriverManager.getConnection(url, username, userpass);
-          String sql = "select count(*) from mini_member where id = ?";
-          pstmt = con.prepareStatement(sql);
-          pstmt.setString(1, id);
-          rs = pstmt.executeQuery();
-          if(rs.next() && rs.getInt(1) == 1) {
-              result = true;
-          }
-      }finally {
-          closeAll(rs, pstmt, con);
-      }
-      return result;
-  }
+    public void register(MemberVO vo) throws SQLException {
+                
+    }
+    
+    public boolean idCheck(String id) throws SQLException {
+        boolean result = false;
+        Connection con = null;
+        PreparedStatement pstmt =  null;
+        ResultSet rs = null;
+        try {
+            con = DriverManager.getConnection(url, username, userpass);
+            String sql = "select count(*) from mini_member where id = ?";
+            pstmt = con.prepareStatement(sql);
+            pstmt.setString(1, id);
+            rs = pstmt.executeQuery();
+            if(rs.next() && rs.getInt(1) == 1) {
+                result = true;
+            }
+        }finally {
+            closeAll(rs, pstmt, con);
+        }
+        return result;
+    }
+    public void updateMemberInfo(MemberVO vo) throws SQLException {
+        Connection con = null;
+        PreparedStatement pstmt = null;
+        try {
+            con = DriverManager.getConnection(url, username, userpass);
+            StringBuilder sql = new StringBuilder("update mini_member set password=?, ");
+            sql.append("email=?, name=?, address=?, birthday=? where id=?");
+            pstmt = con.prepareStatement(sql.toString());
+            pstmt.setString(1, vo.getName());
+            pstmt.setString(2, vo.getEmail());
+            pstmt.executeQuery();
+        } finally {
+            closeAll(pstmt, con);
+        }
+    }
 }
- 
